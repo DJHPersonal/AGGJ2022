333bc9ce6ecd79475852ae287efcbc219bd38956
DJHPersonal <69775476+DJHStudent@users.noreply.github.com>
2022-01-28 19:39:06 +1100

    Added Basic Unreal File

---
+++ Source/FrogKnight/FrogKnightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FrogKnightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROGKNIGHT_API AFrogKnightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
